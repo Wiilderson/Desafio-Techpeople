1c8604670ece12e53ad194fd50c2309f18700089
Wilderson Marreiro <wildersonazevedo@yahoo.com.br>
2022-04-05 16:56:32 -0300

    Parte 1 da tela Compromisso e cards concluída.

--- src/Components/Commitment/Compromisso.tsx
+++ src/Components/Commitment/Compromisso.tsx
@@ -1,10 +1,82 @@
-import { Container, Divsplit } from "./Styled";
+import {
+  CardBloco,
+  CardsCompromisse,
+  Container,
+  Divsplit,
+  Img,
+  Paragrf,
+  Paragrf2,
+  Span,
+  Span2,
+  TextCompromisse,
+} from "./Styled";
+import Escudo from "../../Assets/Group.svg";
+import Stars from "../../Assets/stars1.svg";
+import EscudoRed from "../../Assets/Rectangle.svg";
+import School from "../../Assets/school.svg";
+import EscudoVerde from "../../Assets/Escudoverde.svg";
+import Check from "../../Assets/Check.svg";
 
 function Compromisso() {
   return (
     <>
       <Container>
         <Divsplit></Divsplit>
+        <TextCompromisse>
+          <h1>Nosso Comprimisso</h1>
+          <p>
+            Temos um compromisso de fazer desafios e gerar avaliações precisas
+            para que você saiba o quanto está evoluíndo. Acreditamos que você
+            merece uma avaliação justa e precisa do seu processo.
+          </p>
+        </TextCompromisse>
+        <CardsCompromisse>
+          <CardBloco>
+            <Img src={Escudo} alt="escudo de fundo" />
+            <Span>
+              <img src={Stars} alt="estrela central" />
+            </Span>
+            <h4>Avaliar Positivamente</h4>
+
+            <Paragrf>
+              <p>
+                Entendemos que você está aprendendo. A gente também passou por
+                esta fase, e sabemos que as coisas mudam muito rápido.
+              </p>
+            </Paragrf>
+          </CardBloco>
+
+          <CardBloco>
+            <Img src={EscudoRed} alt="escudo de fundo red" />
+            <Span>
+              <img src={School} alt="escola chapeu" />
+            </Span>
+            <h4>Apoio pós-desafio</h4>
+
+            <Paragrf>
+              <p>
+                Você saberá como está evoluindo, e nós aprendemos a deixar o
+                nosso processo avaliativo mais seguro e de confiança.
+              </p>
+            </Paragrf>
+          </CardBloco>
+
+          <CardBloco>
+            <Img src={EscudoVerde} alt="escudo de fundo" />
+            <Span2>
+              <img src={Check} alt="estrela central" />
+            </Span2>
+            <h4>Avaliar o que faz sentido</h4>
+
+            <Paragrf2>
+              <p>
+                Nada de algoritmo de número primo ou de árvore binária. Nossos
+                desafios se basearam no que você irá realmente trabalhar no
+                futuro.
+              </p>
+            </Paragrf2>
+          </CardBloco>
+        </CardsCompromisse>
       </Container>
     </>
   );
--- src/Components/Commitment/Styled.ts
+++ src/Components/Commitment/Styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Divsplit = styled.div`
   position: absolute;
   width: 56%;
-  height: 200px;
+  height: 100px;
   background-color: #f2f2f2;
   margin-left: 838px;
   margin-top: -32px;
@@ -18,3 +18,138 @@ export const Container = styled.div`
   background-color: #f2f2f2;
   /* max-width: 1182px; */
 `;
+
+export const TextCompromisse = styled.div`
+  width: 949px;
+  margin: auto;
+  h1 {
+    margin-top: 60px;
+    position: relative;
+    text-align: center;
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 37px;
+    line-height: 54px;
+  }
+
+  p {
+    text-align: center;
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 38px;
+    color: #7d7d7d;
+  }
+`;
+
+export const CardsCompromisse = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  width: 1120px;
+  background-color: green;
+  height: 755px;
+  margin-top: 90px;
+`;
+
+export const CardBloco = styled.div`
+  width: 344px;
+  height: 351px;
+  background: #ffffff;
+  box-shadow: 0px 0px 77px rgba(0, 0, 0, 0.06);
+  border-radius: 15px;
+  /* margin-top: 10px; */
+
+  h4 {
+    margin-top: 30px;
+    margin-left: 31px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+    letter-spacing: 0.444444px;
+  }
+  p {
+    margin-top: 10px;
+    margin-left: 31px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 28px;
+    letter-spacing: 0.444444px;
+    color: rgba(55, 71, 79, 0.7216);
+  }
+`;
+
+export const Span = styled.span`
+  img {
+    margin-left: -62px;
+    margin-bottom: 14px;
+  }
+`;
+
+export const Img = styled.img`
+  margin: 29px 0 0 31px;
+`;
+
+export const Paragrf = styled.div`
+  width: 316px;
+`;
+
+export const Span2 = styled.span`
+  img {
+    margin-left: -54px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const Paragrf2 = styled.div`
+  width: 331px;
+`;
+//------------------------------------------------------------------------------------------------
+
+// export const CardBloco2 = styled.div`
+//   width: 344px;
+//   height: 351px;
+//   background: #ffffff;
+//   box-shadow: 0px 0px 77px rgba(0, 0, 0, 0.06);
+//   border-radius: 15px;
+//   /* margin-top: 10px; */
+
+//   h4 {
+//     margin-top: 30px;
+//     margin-left: 31px;
+//     font-family: "Roboto", sans-serif;
+//     font-weight: 700;
+//     font-size: 24px;
+//     line-height: 28px;
+//     letter-spacing: 0.444444px;
+//   }
+//   p {
+//     margin-top: 10px;
+//     margin-left: 31px;
+//     font-family: "Roboto", sans-serif;
+//     font-weight: 500;
+//     font-size: 16px;
+//     line-height: 28px;
+//     letter-spacing: 0.444444px;
+//     color: rgba(55, 71, 79, 0.7216);
+//   }
+// `;
+
+// export const Span2 = styled.span`
+//   img {
+//     margin-left: -62px;
+//     margin-bottom: 14px;
+//   }
+// `;
+
+// export const Img2 = styled.img`
+//   margin: 29px 0 0 31px;
+// `;
+
+// export const Paragrf2 = styled.div`
+//   width: 316px;
+// `;
