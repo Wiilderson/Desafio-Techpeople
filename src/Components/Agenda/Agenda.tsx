5c9b3be9430c41942ff290f9a4304ccbeeb2e996
Wilderson Marreiro <wildersonazevedo@yahoo.com.br>
2022-04-26 17:33:40 -0300

    Parte 2 do componente Agenda, concluído o conteúdo do card.

--- src/Components/Agenda/Agenda.tsx
+++ src/Components/Agenda/Agenda.tsx
@@ -1,7 +1,12 @@
 import {
   CardAgenda,
+  CardContent,
   CardsAgendaContainer,
   Container,
+  Data,
+  HoursContent,
+  LiveText,
+  Paragrf,
   TextAgenda,
 } from "./Styled";
 
@@ -17,7 +22,28 @@ function Agenda() {
       </TextAgenda>
 
       <CardsAgendaContainer>
-        <CardAgenda>a</CardAgenda>
+        <CardAgenda>
+          <CardContent>
+            <Data>
+              <span>20</span>
+              <p>Jun</p>
+            </Data>
+            <LiveText>
+              Live: Iniciando com Nuxt por Mateus Ávila Isidoro
+            </LiveText>
+          </CardContent>
+          <Paragrf>
+            <p>
+              Maiores voluptas laboriosam non dolorum perferendis fuga repellat
+              aut. Blanditiis quos in minus. Voluptatum quia quia voluptas
+              voluptatem vero ex possimus. Iure et consectetur;
+            </p>
+          </Paragrf>
+          <HoursContent>
+            <p>Participar</p>
+            <span>19:00 - 22:00</span>
+          </HoursContent>
+        </CardAgenda>
         <CardAgenda>a</CardAgenda>
       </CardsAgendaContainer>
     </Container>
--- src/Components/Agenda/Styled.ts
+++ src/Components/Agenda/Styled.ts
@@ -48,10 +48,101 @@ export const CardsAgendaContainer = styled.div`
   height: 390px;
   margin-top: 30px;
 `;
-export const CardAgenda = styled.div`
+export const CardAgenda = styled.figure`
   width: 540px;
   height: 308px;
   background: #ffffff;
   box-shadow: 0px 0px 77px rgba(0, 0, 0, 0.06);
   border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardContent = styled.div`
+  width: 446px;
+  height: 78px;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0 0 29px;
+`;
+
+export const Data = styled.div`
+  width: 76px;
+  height: 76px;
+  justify-content: center;
+  background: rgb(113, 124, 255, 0.14);
+  border-radius: 11px;
+  display: flex;
+  flex-direction: column;
+
+  span {
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 27px;
+    line-height: 32px;
+    text-align: center;
+    color: #717cff;
+  }
+  p {
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+    color: #717cff;
+  }
+`;
+
+export const LiveText = styled.h3`
+  width: 350px;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 37px;
+  color: #37474f;
+`;
+
+export const Paragrf = styled.div`
+  width: 481px;
+  height: 91px;
+  margin: 28px 0 0 29px;
+
+  p {
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 28px;
+    letter-spacing: 0.444444px;
+    color: rgba(55, 71, 79, 0.721569);
+  }
+`;
+
+export const HoursContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 481px;
+  margin: 28px 0 0 29px;
+  p {
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    letter-spacing: 0.439286px;
+    color: #717cff;
+  }
+
+  span {
+    font-family: "Roboto", sans-serif;
+    font-weight: 900;
+    font-size: 16px;
+    line-height: 28px;
+    text-align: right;
+    letter-spacing: 0.444444px;
+    color: #373737;
+  }
 `;
